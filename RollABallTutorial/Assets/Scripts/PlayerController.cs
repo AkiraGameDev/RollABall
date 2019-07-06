f151afff0f234b3242f3b22a6b4829a70ba7960a
Christian Orellana <christianorellana097@gmail.com>
2019-07-05 22:11:16 -0400

    UI, baybEEE! it scales too...

--- RollABallTutorial/Assets/Scripts/PlayerController.cs
+++ RollABallTutorial/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
-    void Start(){
+    void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate() {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -21,9 +27,18 @@ public class PlayerController : MonoBehaviour {
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other){
+    void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Pick Up")){
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12) {
+            winText.text = "You Win!";
         }
     }
 }
