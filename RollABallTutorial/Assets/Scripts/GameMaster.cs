6f0051ff61779d19d7acf5043d1cbb359a69ecd5
Christian Orellana <christianorellana097@gmail.com>
2019-07-09 23:57:16 -0400

    Made a little thing

---
+++ RollABallTutorial/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMaster : MonoBehaviour
+{
+    public GameObject playerObj;
+    public Text narratorT;
+    public int pipcount;
+    public float stopwatch;
+    public GameObject[] pips = new GameObject[10];
+
+    private PlayerController playerScript;
+    private bool jumpStart;
+    private bool upStart;
+
+    void Awake(){
+        playerObj = GameObject.Find("Player");
+        playerScript = playerObj.GetComponent<PlayerController>();
+        jumpStart = false;
+        upStart = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopwatch = Time.time;
+        StartCoroutine("Commence");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        pipcount = playerScript.GetCount();
+        if(pipcount == 1 && !jumpStart){
+            jumpStart = true;
+            stopwatch = Time.time;
+            StartCoroutine("JumpTime");
+        }
+        if(pipcount == 3 && !upStart){
+            upStart = true;
+            stopwatch = Time.time;
+            StartCoroutine("UpTime");
+        }
+    }
+
+    IEnumerator Commence(){
+        while(Time.time <= 3.55f){
+            if(Time.time > 0.5f){narratorT.text = "GET";}
+            if(Time.time > 1.0f){narratorT.text = "THOSE";}
+            if(Time.time > 1.5f){narratorT.text = "PIPS";}
+            if(Time.time > 2.0f){narratorT.text = "DONT";}
+            if(Time.time > 2.5f){narratorT.text = "ASK";}
+            if(Time.time > 3.0f){narratorT.text = "QUESTIONS";}
+            if(Time.time > 3.5f){
+                narratorT.text = "";
+                pips[0].SetActive(true);
+            }
+            yield return null;
+        }
+
+    }
+    IEnumerator JumpTime(){
+        while(GetTime() <= 3.6f){
+            if(GetTime() > 0.5f){narratorT.text = "LETS";}
+            if(GetTime() > 1.0f){narratorT.text = "HOPE";}
+            if(GetTime() > 1.5f){narratorT.text = "YOUR";}
+            if(GetTime() > 2.0f){narratorT.text = "SPACE";}
+            if(GetTime() > 2.5f){narratorT.text = "BAR";}
+            if(GetTime() > 3.0f){narratorT.text = "WORKS";}
+            if(GetTime() > 3.5f){
+                narratorT.text = "";
+                pips[1].SetActive(true);
+                pips[2].SetActive(true);
+            }
+            yield return null;
+        }
+
+    }
+    IEnumerator UpTime(){
+        while(GetTime() <= 3.6f){
+            if(GetTime() > 0.5f){narratorT.text = "PATIENCE";}
+            if(GetTime() > 1.0f){narratorT.text = "IS";}
+            if(GetTime() > 1.5f){narratorT.text = "KEY";}
+            if(GetTime() > 2.0f){narratorT.text = "PRESS Q";}
+            if(GetTime() > 2.5f){narratorT.text = "TO";}
+            if(GetTime() > 3.0f){narratorT.text = "BREAK";}
+            if(GetTime() > 3.5f){
+                narratorT.text = "";
+                pips[3].SetActive(true);
+                pips[4].SetActive(true);
+            }
+            yield return null;
+        }
+
+    }
+    IEnumerator FastTime(){
+        while(GetTime() <= 3.6f){
+            if(GetTime() > 0.5f){narratorT.text = "READY";}
+            if(GetTime() > 1.0f){narratorT.text = "TO";}
+            if(GetTime() > 1.5f){narratorT.text = "RUN?";}
+            if(GetTime() > 2.0f){narratorT.text = "PRESS SHIFT";}
+            if(GetTime() > 2.5f){narratorT.text = "TO";}
+            if(GetTime() > 3.0f){narratorT.text = "SPRINT";}
+            if(GetTime() > 3.5f){narratorT.text = "RED = BAD";}
+            yield return null;
+        }
+
+    }
+
+    float GetTime(){
+        return Time.time - stopwatch;
+    }
+}
--- RollABallTutorial/Assets/Scripts/PlayerController.cs
+++ RollABallTutorial/Assets/Scripts/PlayerController.cs
@@ -1,49 +1,142 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     
-    public float speed;
     public Text countText;
     public Text winText;
+    public Text livesText;
+    public GameObject Trail;
+    public float sprintSpeed;
+    public float normalSpeed;
+    public float jumpHeight;
+    public float breakSpeed;
 
     private Rigidbody rb;
+    private Transform tf;
+    private TrailRenderer trailRenderer;
+    private bool isJumping;
+    private bool canJump;
+    private float stamina;
+    private float speed;
+    private float airSprintSpeed;
+    private float airNormalSpeed;
+    private float curSprintSpeed;
+    private float curNormalSpeed;
     private int count;
+    private int staminaDepletion;
+    private int staminaRegen;
+    private int lives;
+    private int hitnum;
 
-    void Start() {
+    void Awake() {
         rb = GetComponent<Rigidbody>();
+        tf = GetComponent<Transform>();
+        trailRenderer = Trail.GetComponent<TrailRenderer>();
         count = 0;
+        stamina = 100;
+        staminaDepletion = 25;
+        staminaRegen = 50;
+        airNormalSpeed = normalSpeed/2;
+        airSprintSpeed = sprintSpeed/2;
+        curSprintSpeed = sprintSpeed;
+        curNormalSpeed = normalSpeed;
+        isJumping = false;
+        canJump = true;
+        lives = 3;
         SetCountText();
+        SetLivesText();
     }
 
     void Update() {
         if (Input.GetKey("escape"))
-            Application.Quit();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);;
+        if(Input.GetKey("left shift") && stamina > 0){
+            speed = curSprintSpeed;
+            trailRenderer.emitting = true;
+            stamina -= Time.deltaTime * staminaDepletion;
+        }
+        else if(!Input.GetKey("left shift") || stamina <= 0){
+            speed = curNormalSpeed;
+            trailRenderer.emitting = false;
+
+            if(stamina < 100 && !Input.GetKey("left shift")) {
+                stamina += Time.deltaTime * staminaRegen;
+            }
+        }
+        if(Input.GetKey("q") && !isJumping){rb.AddForce(rb.velocity*-1*breakSpeed);}
     }
 
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        float jumpAxis = Input.GetAxis("Jump");
 
         Vector3  movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Vector3 jumpVector = new Vector3 (0.0f, jumpAxis, 0.0f);
 
         rb.AddForce(movement * speed);
+
+
+        if(canJump && jumpAxis > 0){
+            curNormalSpeed = airNormalSpeed;
+            curSprintSpeed = airSprintSpeed;
+            rb.AddForce(jumpVector * jumpHeight);
+            isJumping = true;
+            canJump = false;
+        }
+        //if(canJump && isJumping && jumpAxis >0){
+            //rb.AddForce(movement+jumpVector * jumpHeight);
+            //canJump = false;
+        //}
     }
 
+    void OnCollisionEnter(Collision collision){
+        rb.AddForce(collision.impulse*17);
+        if(Physics.Raycast(tf.position, Vector3.down,1.0f)){
+            curNormalSpeed = normalSpeed;
+            curSprintSpeed = sprintSpeed;
+            canJump = true;
+            isJumping = false;
+        }
+    }
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Pick Up")){
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
         }
+        if(other.gameObject.CompareTag("Enemy")){
+            other.gameObject.SetActive(false);
+            lives--;
+            SetLivesText();
+        }
+        if(other.gameObject.CompareTag("DeathBox")){
+            lives--;
+            tf.position = new Vector3(0.0f, 0.7f, 0.0f);
+            SetLivesText();
+        }
     }
 
     void SetCountText() {
-        countText.text = "Count: " + count.ToString();
+        countText.text = "Pips Collected: " + count.ToString();
         if (count >= 12) {
-            winText.text = "You Win!";
+            winText.text = "You monster...";
         }
+    }
+    void SetLivesText() {
+        livesText.text = "Lives Left: " + lives.ToString();
+        if (lives == 0) {
+            this.gameObject.SetActive(false);
+            winText.text = "Jeez, you're bad at this...";
+        }
+    }
+
+    public int GetCount() {
+        return this.count;
     }
 }
